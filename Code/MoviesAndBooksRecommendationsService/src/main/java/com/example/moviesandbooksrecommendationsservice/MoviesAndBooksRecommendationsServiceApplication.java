a7b79020d4c933b9a09a4c05392eb4312b5390f1
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-11-25 00:07:03 +0300

    Initial java project structure

---
+++ Code/MoviesAndBooksRecommendationsService/src/main/java/com/example/moviesandbooksrecommendationsservice/MoviesAndBooksRecommendationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviesandbooksrecommendationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesAndBooksRecommendationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesAndBooksRecommendationsServiceApplication.class, args);
+	}
+
+}
