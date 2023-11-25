c440c8f40ba239fc29c0ec077708c3e834d6661e
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-11-25 11:12:07 +0300

    Main page and "Movies" page were added and connected.

--- Code/MoviesAndBooksRecommendationsService/src/main/java/com/example/moviesandbooksrecommendationsservice/controller/RecsController.java
+++ Code/MoviesAndBooksRecommendationsService/src/main/java/com/example/moviesandbooksrecommendationsservice/controller/RecsController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RecsController {
-    @GetMapping("/")
+    @GetMapping("/main")
     public String index() {
         return "index.html";
+    }
+
+    @GetMapping("/movies")
+    public String getRecommendations() {
+        return "movies.html";
     }
 }
