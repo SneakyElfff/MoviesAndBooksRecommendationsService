fbbd1b51536c09ae9b0c13e3200b38f841116da4
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-11-25 21:49:51 +0300

    Recovery commit

---
+++ Code/MoviesAndBooksRecommendationsService/src/test/java/com/example/moviesandbooksrecommendationsservice/MoviesAndBooksRecommendationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviesandbooksrecommendationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesAndBooksRecommendationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
