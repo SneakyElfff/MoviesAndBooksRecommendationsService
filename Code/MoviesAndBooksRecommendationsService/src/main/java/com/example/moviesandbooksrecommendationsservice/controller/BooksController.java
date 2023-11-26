87e850e29b192e3a80438846367bd018ee62023a
Nina Alhimovich of 150502 <alhimovich.nina@gmail.com>
2023-11-26 19:35:45 +0300

    Search option added to the "Books" page. Connection to the Google Books API was implemented.

---
+++ Code/MoviesAndBooksRecommendationsService/src/main/java/com/example/moviesandbooksrecommendationsservice/controller/BooksController.java
@@ -0,0 +1,58 @@
+package com.example.moviesandbooksrecommendationsservice.controller;
+
+import com.example.moviesandbooksrecommendationsservice.entities.Book;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BooksController {
+    private static final Logger logger = LoggerFactory.getLogger(BooksController.class);
+
+    private static final String GOOGLE_BOOKS_API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    @GetMapping("/searchBook")
+    public ResponseEntity<Book> getBookInfo(@RequestParam String bookTitle, @RequestParam(required = false) String authorName) {
+        RestTemplate template = new RestTemplate();
+
+        String url = GOOGLE_BOOKS_API_URL + bookTitle + "+intitle:" + bookTitle + "+inauthor:" + authorName;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-API-KEY", "AIzaSyCKmxvPEfqkhJObL5Byce24XmKvmTuo2lY");
+        headers.set("Accept", "application/json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, entity, String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode root = objectMapper.readTree(response.getBody());
+            
+            Book book = null;
+            if (root.path("totalItems").asInt() == 0)
+                return ResponseEntity.ok(book);
+            JsonNode book_node = root.path("items").get(0).path("volumeInfo");
+
+            book = objectMapper.treeToValue(book_node, Book.class);
+
+            logger.info("Deserialized book: {}", book);
+            return ResponseEntity.ok(book);
+        } catch (IOException e) {
+            logger.error("Error deserializing book", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
+
---
+++ Code/MoviesAndBooksRecommendationsService/src/main/java/com/example/moviesandbooksrecommendationsservice/entities/Book.java
@@ -0,0 +1,48 @@
+package com.example.moviesandbooksrecommendationsservice.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Book {
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("authors")
+    private List<String> authors;
+
+    @JsonProperty("description")
+    private String description;
+
+    @JsonProperty("publishedDate")
+    private String publishedDate;
+
+    @JsonProperty("categories")
+    private List<String> categories;
+
+    @JsonProperty("publisher")
+    private String publisher;
+
+    @JsonProperty("maturityRating")
+    private String maturityRating;
+
+    @JsonProperty("pageCount")
+    private int pageCount;
+
+    @JsonProperty("imageLinks")
+    private Thumbnail thumbnail;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Thumbnail {
+        @JsonProperty("thumbnail")
+        private String thumbnail;
+
+        public void setThumbnail(String thumbnail) {
+            this.thumbnail = thumbnail;
+        }
+    }
+}
